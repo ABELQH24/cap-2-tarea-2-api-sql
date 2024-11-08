4325a4f2c23c138af579d51ba69b9b2db4dcf9e8
unknown <qabelisaias@gmail.com>
2024-11-07 22:18:20 -0500

    fix: error de post_id corregido

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
 import sqlite3
 from flask import Flask, render_template, abort, request, url_for, redirect
-from db import get_db_connection
+# from db import get_db_connection
+
+def get_db_connection():
+    conn = sqlite3.connect("database.db")
+    conn.row_factory =sqlite3.Row
+    return conn
+    
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-        return render_template('base.html')
+        return render_template('home.html')
 
 @app.route('/home', methods=['GET'])
 def home():
@@ -15,7 +21,7 @@ def home():
 @app.route('/post', methods=['GET'])
 def get_all_post():
     conn = get_db_connection()
-    posts = conn.execute('SELECT * FROM posts').fetchone()
+    posts = conn.execute('SELECT * FROM posts').fetchall()
     conn.close()
     return render_template('post/posts.html', posts=posts)
 
@@ -40,10 +46,10 @@ def create_one_post():
     return render_template('post/create.html')
 
 
-@app.route('/post/edit/<int:id>', methods=['GET', 'POST'])
-def edit_one_post(id):
+@app.route('/post/edit/<int:post_id>', methods=['GET', 'POST'])
+def edit_one_post(post_id):
     conn = get_db_connection()
-    post = conn.execute('SELECT * FROM posts WHERE id = ?', (id,)).fetchone()
+    post = conn.execute('SELECT * FROM posts WHERE id = ?', (post_id,)).fetchone()
     conn.close()
     
     if request.method == 'POST':
@@ -51,7 +57,7 @@ def edit_one_post(id):
         content = request.form['content']
 
         conn = get_db_connection()
-        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, id))
+        conn.execute('UPDATE posts SET title = ?, content = ? WHERE id = ?', (title, content, post_id))
         conn.commit()
         conn.close()
         return redirect(url_for('get_all_post'))
@@ -60,10 +66,10 @@ def edit_one_post(id):
         return render_template('post/edit.html', post=post)
 
 
-@app.route('/post/delete/<post_id>', methods=['POST'])
+@app.route('/post/delete/<int:post_id>', methods=['POST'])
 def delete_one_post(post_id):
     conn = get_db_connection()
-    conn.execute('DELETE FROM posts WHERE id = ?', (post_id))
+    conn.execute('DELETE FROM posts WHERE id = ?', (post_id,))
     conn.commit()
     conn.close()
 
